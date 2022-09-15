d1bee75e9793aeb7963a19737817f218391f5435
Harveen G <harveen096@revature.net>
2022-09-15 15:21:30 -0700

    changed the database and everything should work

--- src/main/java/DAO/databaseFunctions.java
+++ src/main/java/DAO/databaseFunctions.java
@@ -6,6 +6,8 @@ import Util.connectionUtil;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class databaseFunctions {
     Connection conn= connectionUtil.getConnection();
@@ -18,30 +20,60 @@ public class databaseFunctions {
         }
         return false;
     }
+
+    public String getItemIsle(String name){
+        List<item> itemList=returnAllItems();
+        for(int i=0; i< itemList.size(); i++){
+            String currentName= itemList.get(i).item_name;
+            if(currentName.equals(name)){
+                return itemList.get(i).isle;
+            }
+        }
+        return "";
+    }
+
+
+    public List<item> connectLists(List<item> one,List<item> two ){
+       List<item> newList = Stream.concat(one.stream(), two.stream())
+               .collect(Collectors.toList());
+       return newList;
+    }
    public  List<item> returnItemsInIsle(String isleName){
-       List<item> groceryItemArray = new ArrayList<>();
-       try {
-           PreparedStatement statement = conn.prepareStatement("Select * From groceryItems where isle = ?");
-           statement.setString(1, isleName);
-           ResultSet rs = statement.executeQuery();
-           while(rs.next()){
-               item groceryItem = new item(rs.getString("item_name"), rs.getString("isle"),
-                       rs.getInt("quantity"),rs.getFloat("price"));
-               groceryItemArray.add(groceryItem);
-           }
-       }catch(SQLException e){
-           e.printStackTrace();
-       }
-       return groceryItemArray;
+      if(isleName.equals("produce")){
+          return returnAllProduceItems();
+      }else if(isleName.equals("bakery")){
+          return returnAllBakeryItems();
+      }else if(isleName.equals("meat")){
+          return returnAllMeatItems();
+      }else if(isleName.equals("dairy")){
+          return returnAllDairyItems();
+      }else if(isleName.equals("convenience")){
+          return returnAllConvenienceItems();
+      }else{
+          return null;
+      }
    }
 
     public  List<item> returnAllItems(){
+        List<item> produce = returnAllProduceItems();
+        List<item> bakery = returnAllBakeryItems();
+        List<item> pb= connectLists(produce,bakery);
+        List<item> meat = returnAllMeatItems();
+        List<item> pbm=connectLists(pb, meat);
+        List<item> dairy=returnAllDairyItems();
+        List<item> pbmd=connectLists(pbm,dairy);
+        List<item> convenience = returnAllConvenienceItems();
+        return connectLists(pbmd,convenience);
+    }
+    public List<item> returnAllProduceItems(){
         List<item> groceryItemArray = new ArrayList<>();
         try {
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("Select * From groceryItems");
+            ResultSet rs = statement.executeQuery("select groceryItem.item_name, groceryItem.quantity," +
+                    "groceryItem.price from produce  join groceryItem on " +
+                    "groceryItem.item_name= produce.item_name");
             while(rs.next()){
-                item groceryItem = new item(rs.getString("item_name"), rs.getString("isle"),
+                item groceryItem = new item(rs.getString("item_name"), "produce",
                         rs.getInt("quantity"),rs.getFloat("price"));
                 groceryItemArray.add(groceryItem);
             }
@@ -51,6 +83,81 @@ public class databaseFunctions {
         }
         return null;
     }
+
+    public List<item> returnAllBakeryItems(){
+        List<item> groceryItemArray = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("select groceryItem.item_name, groceryItem.quantity," +
+                    "groceryItem.price from bakery  join groceryItem on " +
+                    "groceryItem.item_name= bakery.item_name");
+            while(rs.next()){
+                item groceryItem = new item(rs.getString("item_name"), "bakery",
+                        rs.getInt("quantity"),rs.getFloat("price"));
+                groceryItemArray.add(groceryItem);
+            }
+            return groceryItemArray;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<item> returnAllMeatItems(){
+        List<item> groceryItemArray = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("select groceryItem.item_name, groceryItem.quantity," +
+                    "groceryItem.price from meat join groceryItem on " +
+                    "groceryItem.item_name= meat.item_name");
+            while(rs.next()){
+                item groceryItem = new item(rs.getString("item_name"), "meat",
+                        rs.getInt("quantity"),rs.getFloat("price"));
+                groceryItemArray.add(groceryItem);
+            }
+            return groceryItemArray;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<item> returnAllDairyItems(){
+        List<item> groceryItemArray = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("select groceryItem.item_name, groceryItem.quantity," +
+                    "groceryItem.price from dairy join groceryItem on " +
+                    "groceryItem.item_name= dairy.item_name");
+            while(rs.next()){
+                item groceryItem = new item(rs.getString("item_name"), "dairy",
+                        rs.getInt("quantity"),rs.getFloat("price"));
+                groceryItemArray.add(groceryItem);
+            }
+            return groceryItemArray;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<item> returnAllConvenienceItems(){
+        List<item> groceryItemArray = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("select groceryItem.item_name, groceryItem.quantity," +
+                    "groceryItem.price from convenience join groceryItem on " +
+                    "groceryItem.item_name= convenience.item_name");
+            while(rs.next()){
+                item groceryItem = new item(rs.getString("item_name"), "convenience",
+                        rs.getInt("quantity"),rs.getFloat("price"));
+                groceryItemArray.add(groceryItem);
+            }
+            return groceryItemArray;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
    
    public void addItemToCart(item thing, int amount){
        try{
@@ -66,11 +173,10 @@ public class databaseFunctions {
                updateGroceryStore(thing, getSpecificThing(thing.item_name).quantity - amount);
            }else {
                PreparedStatement statement = conn.prepareStatement("insert into groceryCart(item_name, " +
-                       "isle, quantity, price)" + "values (?, ?,?,?)");
+                       " quantity, price)" + "values (?, ?,?)");
                statement.setString(1, thing.item_name);
-               statement.setString(2, thing.isle);
-               statement.setInt(3, amount);
-               statement.setFloat(4, thing.price);
+               statement.setInt(2, amount);
+               statement.setFloat(3, thing.price);
                statement.executeUpdate();
                //edit the original database for the amount
                updateGroceryStore(thing, getSpecificThing(thing.item_name).quantity - amount);
@@ -82,22 +188,13 @@ public class databaseFunctions {
    }
 
     public item getSpecificThing(String name){
-        try {
-            List<item> groceryItemArray = new ArrayList<>();
-            PreparedStatement statement = conn.prepareStatement("Select * From groceryItems where item_name = ?");
-            statement.setString(1, name);
-            ResultSet rs = statement.executeQuery();
-            while(rs.next()){
-                item groceryItem = new item(rs.getString("item_name"), rs.getString("isle"),
-                        rs.getInt("quantity"),rs.getFloat("price"));
-                groceryItemArray.add(groceryItem);
-            }
-            return groceryItemArray.get(0);
-
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-        return null; //should not get here so it is fine
+      List<item> allItems= returnAllItems();
+      for(int i=0; i< allItems.size(); i++){
+          if(name.equals(allItems.get(i).item_name)){
+              return allItems.get(i);
+          }
+      }
+      return null;
     }
     public void removeItemFromCart(item thing, int quantity){
         try{
@@ -127,7 +224,7 @@ public class databaseFunctions {
 
     public void updateGroceryStore(item thing, int amount){
         try{
-            PreparedStatement statement = conn.prepareStatement("update groceryItems set quantity = ? " +
+            PreparedStatement statement = conn.prepareStatement("update groceryItem set quantity = ? " +
                     "where item_name = ?");
             statement.setInt(1,amount);
             statement.setString(2, thing.item_name);
@@ -144,7 +241,7 @@ public class databaseFunctions {
 
             ResultSet rs = statement.executeQuery();
             while(rs.next()){
-                item groceryItem = new item(rs.getString("item_name"), rs.getString("isle"),
+                item groceryItem = new item(rs.getString("item_name"), getItemIsle(rs.getString("item_name")),
                         rs.getInt("quantity"),rs.getFloat("price"));
                 groceryCartArray.add(groceryItem);
             }
@@ -162,7 +259,7 @@ public class databaseFunctions {
             statement.setString(1, name);
             ResultSet rs = statement.executeQuery();
             while(rs.next()){
-                item groceryItem = new item(rs.getString("item_name"), rs.getString("isle"),
+                item groceryItem = new item(rs.getString("item_name"), getItemIsle(rs.getString("item_name")),
                         rs.getInt("quantity"),rs.getFloat("price"));
                 groceryItemArray.add(groceryItem);
             }
